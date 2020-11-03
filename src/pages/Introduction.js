120d6b88f21cc66abcba07db832d96b2ce63d8f5
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-11-03 22:02:58 +0300

    Topics added

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -1,6 +1,55 @@
 import React from 'react';
 import {SafeAreaView, View, Text} from 'react-native';
 
+const topics = [
+  {
+    id: 0,
+    name: 'Java',
+    color: 'fb5607'
+  },
+  {
+    id: 1,
+    name: 'Python',
+    color: '007f5f'
+  },
+  {
+    id: 2,
+    name: 'Javascript',
+    color: 'ffb703'
+  },
+  {
+    id: 3,
+    name: '.NET',
+    color: '023e7d'
+  },
+  {
+    id: 4,
+    name: 'Dart',
+    color: '001845'
+  },
+  {
+    id: 5,
+    name: 'Go',
+    color: 'f8961e'
+  },
+  {
+    id: 5,
+    name: 'Ruby',
+    color: 'e63946'
+  },
+  {
+    id: 6,
+    name: 'C',
+    color: 'fb8b24'
+  },
+  {
+    id: 7,
+    name: 'C++',
+    color: '06d6a0'
+  },
+
+]
+
 const Introduction = (props) => {
   return (
     <SafeAreaView>
