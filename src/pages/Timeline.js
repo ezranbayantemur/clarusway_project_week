6913691cda7e0317d71c01641210729a7fcab360
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-11-07 21:01:56 +0300

    Backdrop operation added

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,12 +1,39 @@
+import moment from 'moment';
 import React, {useState} from 'react';
+import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
 import {SafeAreaView, View, Text, FlatList} from 'react-native';
 
 import {timelinePage} from './styles';
 import {PostItem, PostInput, Header, TopicSelectModal} from '../components';
 
+const user = auth().currentUser;
+
 const Timeline = () => {
   const [selectedTopic, setSelectedTopic] = useState(null);
-  const [topicModalFlag, setTopicModalFlag] = useState(false);
+  const [topicModalFlag, setTopicModalFlag] = useState(true);
+
+  const selectingTopic = (value) => {
+    setSelectedTopic(value);
+    setTopicModalFlag(false);
+
+    database()
+      .ref()
+      .on('value', (snapshot) => {
+        console.log("UPDATE......")
+        console.log(snapshot.val())
+      });
+  };
+
+  const sendingPost = (value) => {
+    const postObject = {
+      userMail: user.email,
+      postText: value,
+      time: moment().toISOString()
+    }
+
+    database().ref(`${selectedTopic}`).push(postObject);
+  };
 
   return (
     <SafeAreaView style={timelinePage.container}>
@@ -18,15 +45,12 @@ const Timeline = () => {
 
         <FlatList data={[]} renderItem={() => null} />
 
-        <PostInput onSendPost={(value) => console.log(value)} />
+        <PostInput onSendPost={sendingPost} />
 
         <TopicSelectModal
           visibility={topicModalFlag}
-          onClose={() => setTopicModalFlag(false)}
-          onTopicSelect={(value) => {
-            setSelectedTopic(value);
-            setTopicModalFlag(false);
-          }}
+          onClose={() => (selectedTopic ? setTopicModalFlag(false) : null)}
+          onTopicSelect={selectingTopic}
         />
       </View>
     </SafeAreaView>
