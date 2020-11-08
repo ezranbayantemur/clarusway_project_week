adb9f7b1024555dc1934491e409473576fd9fd87
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-11-08 17:08:42 +0300

    Lesson started

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import {initialState} from './context/store';
-import {reducer} from './context/reducer';
-
 import {Intro, Questions, Finish} from './pages';
+
+const Stack = createStackNavigator();
 
 function Router() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Intro" component={Intro} />
-          <Stack.Screen name="Finish" component={Finish} />
-          <Stack.Screen name="Questions" component={Questions} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Intro" component={Intro} />
+        <Stack.Screen name="Finish" component={Finish} />
+        <Stack.Screen name="Questions" component={Questions} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/pages/Finish.js
+++ src/pages/Finish.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {SafeAreaView, View, FlatList, Text, Button} from 'react-native';
-import {useSelector} from 'react-redux';
 
 import {finishPage} from './styles';
 
@@ -8,7 +7,7 @@ const Finish = (props) => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={finishPage.container}>
-        <Text style={finishPage.text}>Your score is {userScore}</Text>
+        <Text style={finishPage.text}>Your score is {0}</Text>
       </View>
     </SafeAreaView>
   );
