5725d3a90c5b5a940d3389f1739d3e90bbfce1f4
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-11-08 18:19:11 +0300

    Connected to API and added timer component

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -7,16 +7,30 @@ import {
   Animated,
 } from 'react-native';
 import {CategorySelectModal} from '../components';
+import {CountdownCircleTimer} from 'react-native-countdown-circle-timer';
 
 import {introPage} from './styles';
+import axios from 'axios';
 
 const Intro = (props) => {
+  const [counterFlag, setCounterFlag] = useState(false);
   const [modalFlag, setModalFlag] = useState(false);
 
   const startGame = (selectedCategory) => {
-    console.log(selectedCategory);
-    setModalFlag(false)
-  }
+    axios.get(`https://opentdb.com/api.php?`, {
+      params: {
+        amount: 10,
+        category: selectedCategory.id,
+        type: "boolean",
+      },
+    })
+    .then(res => {
+      console.log(res);
+    });
+
+    setModalFlag(false);
+    setCounterFlag(true);
+  };
 
   return (
     <SafeAreaView style={{flex: 1}}>
@@ -24,6 +38,24 @@ const Intro = (props) => {
         <View style={introPage.container}>
           <Text style={introPage.bannerText}>Trivia!</Text>
         </View>
+
+        <View style={{backgroundColor: '#3949ab', alignItems: 'center'}}>
+          <CountdownCircleTimer
+            isPlaying={counterFlag}
+            duration={5}
+            colors={[
+              ['#fff176', 0.4],
+              ['#ba68c8', 0.4],
+              ['#ff8a65', 0.2],
+            ]}>
+            {({remainingTime, animatedColor}) => (
+              <Animated.Text style={{fontSize: 60, color: animatedColor}}>
+                {remainingTime}
+              </Animated.Text>
+            )}
+          </CountdownCircleTimer>
+        </View>
+
         <View style={introPage.container}>
           <TouchableOpacity
             style={introPage.buttonContainer}
@@ -32,7 +64,7 @@ const Intro = (props) => {
           </TouchableOpacity>
         </View>
 
-        <CategorySelectModal 
+        <CategorySelectModal
           visibility={modalFlag}
           onClose={() => setModalFlag(false)}
           onCategorySelect={startGame}
